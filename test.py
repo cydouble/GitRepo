369299a54cd3aeeb01bcfb03e0ea18191e9c09ad
cyy <vissititute@163.com>
2018-09-13 08:26:07 +0000

    written test.py

---
+++ test.py
@@ -0,0 +1,12 @@
+
+rdCount(data):
+    re = {}
+    for i in data:
+        re[i] = re.get(i, 0 ) + 1
+    return re
+
+if __name__ == "__main__":
+    data = ["ab","cd","ab","d","d"]
+    print ("The result is %s" % wordCount(data))
+    
+
